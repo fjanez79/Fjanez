750d343f91a9cddbb6aab5a84251639b4e17d47a
fjanez79 <77860670+fjanez79@users.noreply.github.com>
2021-01-22 18:30:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+var tmi = require('tmi.js');
+var readline = require('readline');
+var https = require('https');
+
+var canais = [];
+var client = null;
+
+
+
+executar_canais();
+conectar();
+
+//setInterval(function(){
+
+    //client.whisper("johnpittertvbot", "!bonus");
+	
+//}, 10 * 60 * 1000);
+
+setInterval(function(){
+
+    canais = [];
+
+    client.disconnect();
+
+    executar_canais();
+    conectar();
+	
+}, 50 * 60 * 1000);
+
+
+
+
+function executar_canais() {
+    var site = https.get("https://raw.githubusercontent.com/glaydson2323/glaydson2323/main/canais.txt", function(pagina) {
+    var github = readline.createInterface({
+      input: pagina
+    });
+    github.on('line', (line) => {
+      canais.push(line);
+    }).on('close', () => {
+      console.log(canais);
+    });
+  });
+  }
+
+function conectar() {
+    var options = {
+        options: {
+            debug: true
+        },
+        connection: {
+            cluster: "aws",
+            reconnect: true
+        }, 
+        identity: {
+            username: "hell_play_",
+            password: "oauth:h77j2l08av4z4ytpi88e6swchh2oy7"
+        },
+        channels: canais
+    };
+    
+    client = new tmi.client(options);
+    client.connect();
+    
+    client.on("connected", function (address, port) {
+        console.log("Conectado...");
+    });
+    
+}
+
